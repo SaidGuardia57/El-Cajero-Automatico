7935e527e10dad43f69ace7ed67adc8a43b15f26
David <david.pilo@uip.edu.pa>
2026-03-26 10:57:44 -0500

    se agrego deposito

--- depósito.py
+++ depósito.py
@@ -1,1 +1,15 @@
+def depositar(saldo, historial):
+    try:
+        monto = float(input("Ingresa el monto a depositar: "))
+    except:
+        print("Monto inválido\n")
+        return saldo
 
+    if monto > 0:
+        saldo += monto
+        historial.append(f"Depósito: +${monto}")
+        print(f"Depósito exitoso. Nuevo saldo: ${saldo}\n")
+    else:
+        print("El monto debe ser mayor a 0\n")
+
+    return saldo
