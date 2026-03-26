da4e7f5a1d578da4fab633075a3340a31b29705d
jerry <jerry.cedeno@uip.edu.pa>
2026-03-25 19:38:29 -0500

    agrega codigo de historial

--- historial.py
+++ historial.py
@@ -1,1 +1,9 @@
+def ver_historial(historial):
+    print("\n--- HISTORIAL ---")
 
+    if not historial:
+        print("No hay movimientos\n")
+    else:
+        for movimiento in historial:
+            print(movimiento)
+        print()
