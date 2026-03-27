2dd2adbb0f594405be3d412549f99cdd88ffbda3
Gian <gian.pitti@uip.edu.pa>
2026-03-26 23:13:46 -0500

    Se agrega login

--- login.py
+++ login.py
@@ -1,1 +1,17 @@
+#login.py
 
+def iniciar_sesion(usuarios):
+    intentos = 3
+
+    while intentos > 0:
+        pin = input("Ingresa tu PIN: ")
+
+        if pin in usuarios:
+            print("Acceso concedido\n")
+            return pin  # usuario activo
+        else:
+            intentos -= 1
+            print(f"PIN incorrecto. Intentos restantes: {intentos}\n")
+
+    print("Acceso bloqueado")
+    return None
