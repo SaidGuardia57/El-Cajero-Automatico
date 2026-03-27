18690a13e8c4daaa40ebfe6d86bd91b3144a95d5
Maria-uip <maria.cedenor@uip.edu.pa>
2026-03-26 21:06:44 -0500

    Retiro

--- retiro.py
+++ retiro.py
@@ -1,1 +1,19 @@
+#retiro.py
 
+def retirar(saldo, historial):
+    try:
+        monto = float(input("Ingresa el monto a retirar: "))
+    except:
+        print("Monto inválido\n")
+        return saldo
+
+    if monto > saldo:
+        print("Fondos insuficientes\n")
+    elif monto <= 0:
+        print("Monto inválido\n")
+    else:
+        saldo -= monto
+        historial.append(f"Retiro: -${monto}")
+        print(f"Retiro exitoso. Nuevo saldo: ${saldo}\n")
+
+    return saldo
